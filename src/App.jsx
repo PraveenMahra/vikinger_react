ad1e07320b22a2c123e1c34f4030ee3945a6184d
PraveenMahra <praveen.mehra65@gmail.com>
2024-03-02 14:32:09 +0530

    add scrool to top function when we navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/home/homePage";
 import TestimonialPage from "./pages/testimonial/testimonialPage";
 import SignIn from "./pages/signIn/signInPage";
 import ForgetPassword from "./pages/forgetPassword/forgetPassword";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="testimonials" element={<TestimonialPage />} />
